f99b128bc56362c974a7dd76da7a8f1db237d0f2
CaptainAdil <adil.nabiyev@bk.ru>
2022-01-24 17:06:46 +0400

    Added dto and validation annotations

--- src/main/java/com/example/idcard/dto/PersonDto.java
+++ src/main/java/com/example/idcard/dto/PersonDto.java
@@ -1,18 +1,9 @@
 package com.example.idcard.dto;
 
 
-import lombok.Data;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 
 
 public interface PersonDto {
-
 
     public String getFirstName();
     public String getLastName();
